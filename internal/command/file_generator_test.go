7762d6d9ee3257d9e6d60241cc469773b94dc86e
Bong Nguyen <van.bong@grabtaxi.com>
2020-03-05 21:49:15 +0800

    feat: add scaffold command

---
+++ internal/command/file_generator_test.go
@@ -0,0 +1,55 @@
+package command
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type bufWriter struct {
+	b bytes.Buffer
+}
+
+func (w *bufWriter) Write(path string, data []byte) error {
+	_, _ = w.b.Write([]byte(path + ":\n"))
+	_, err := w.b.Write(data)
+	return err
+}
+
+func Test_fileGenerator(t *testing.T) {
+	mockWriter := &bufWriter{}
+	generator := &fileGenerator{
+		filePath:     "main.go",
+		templateName: "main",
+		service: &Service{
+			Package:     "github.com/hello",
+			PackageName: "hello",
+		},
+		writer: mockWriter,
+	}
+
+	err := generator.do()
+	require.NoError(t, err)
+	require.Equal(t,
+		`main.go:
+package main
+
+import (
+	"log"
+
+	"github.com/hello/internal/handlers"
+	"github.com/hello/internal/server"
+)
+
+func main() {
+	err := server.Serve(
+		handlers.New(),
+	)
+
+	log.Println("Service stopped with:", err)
+}
+`,
+		mockWriter.b.String(),
+	)
+}
