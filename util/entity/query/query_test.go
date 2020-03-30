786e263e2281e45df5ecddc6634b37a36f7ca0b5
Bong Nguyen <van.bong@grabtaxi.com>
2020-03-30 17:53:17 +0800

    feat: add mising codes

--- util/entity/query/query_test.go
+++ util/entity/query/query_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/stretchr/testify/require"
 )
 
@@ -15,4 +16,49 @@ func Test_Transform(t *testing.T) {
 
 	newDB := Transform(nil, mockQuery)
 	require.Equal(t, expectedDB, newDB)
+}
+
+func Test_Equal(t *testing.T) {
+	db, err := gorm.Open("sqlite3", ":memory:")
+	require.NoError(t, err)
+	defer db.Close()
+
+	db.AutoMigrate(&gorm.Model{})
+	db.Create(&gorm.Model{ID: 1})
+
+	var item gorm.Model
+	result := Equal("id", 1)(db).Find(&item)
+	require.NoError(t, result.Error)
+	require.EqualValues(t, 1, item.ID)
+}
+
+func Test_Limit(t *testing.T) {
+	db, err := gorm.Open("sqlite3", ":memory:")
+	require.NoError(t, err)
+	defer db.Close()
+
+	db.AutoMigrate(&gorm.Model{})
+	db.Create(&gorm.Model{ID: 1})
+	db.Create(&gorm.Model{ID: 2})
+
+	var items []*gorm.Model
+	result := Limit(1)(db).Find(&items)
+	require.NoError(t, result.Error)
+	require.Len(t, items, 1)
+}
+
+func Test_Offset(t *testing.T) {
+	db, err := gorm.Open("sqlite3", ":memory:")
+	require.NoError(t, err)
+	defer db.Close()
+
+	db.AutoMigrate(&gorm.Model{})
+	db.Create(&gorm.Model{ID: 1})
+	db.Create(&gorm.Model{ID: 2})
+
+	var items []*gorm.Model
+	result := Limit(1)(Offset(1)(db)).Find(&items)
+	require.NoError(t, result.Error)
+	require.Len(t, items, 1)
+	require.EqualValues(t, 2, items[0].ID)
 }
