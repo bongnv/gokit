96111de038161e2b43d79422cf8a1c56cb5f9613
Bong Nguyen <bongnv@users.noreply.github.com>
2020-04-18 16:23:29 +0800

    docs: add introduction (#49)

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,21 @@
 module.exports = {
-    base: "/gokit/",
-    title: "gokit",
-    themeConfig: {
-        repo: "bongnv/gokit",
-        editLinks: true,
-        sidebar: 'auto'
-    }
+  base: "/gokit/",
+  title: "gokit",
+  themeConfig: {
+    repo: "bongnv/gokit",
+    editLinks: true,
+    sidebar: [{
+      title: "Guide",
+      collapsable: false,
+      sidebarDepth: 2,
+      children: [
+        '',
+        'guide',
+        'scaffolding-projects',
+        'scaffolding-crud-endpoints',
+        'generating-services',
+        'generating-dao'
+      ]
+    }]
+  }
 }
